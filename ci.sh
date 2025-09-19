78e690542406e521ae6f92c7728fe189a52cb42d
Neiroleptik <97061340+Neiroleptik@users.noreply.github.com>
2025-09-20 01:10:14 +0300

    Update ci.sh
    excluded bin

--- ci.sh
+++ ci.sh
@@ -7,8 +7,8 @@ cargo build
 echo "Checking formatting..."
 cargo fmt -- --check
 
-echo "Running Clippy..."
-cargo clippy -- -D warnings
+echo "Running Clippy Lib (exclude bin)..."
+cargo clippy --fix --lib -p  pacifica_rust_sdk
 
 echo "Running tests..."
 cargo test
